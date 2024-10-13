eaaaea67f9eae3365cc53829691ce0e64333e1ef
kai713 <kairgaliev713@gmail.com>
2024-10-14 00:58:49 +0500

    separated pages and final project ver 2

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ let section = document.querySelectorAll('.section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach((section) => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = window.offsetHeight;
-        let id = sec.get.getAttribute('id');
+        let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
             })
         }
     })
 }
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelectorAll('.navbar a').forEach(item => item.classList.remove('active'));
+
+        this.classList.add('active');
+    });
+});
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
